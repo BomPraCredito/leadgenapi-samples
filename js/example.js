1f74dae59e6ec2bc1cf2f7a6aa62b14a3b1fd0fb
dpolistbpc <danielp@bompracredito.com.br>
2015-06-09 14:13:19 -0300

    Alteração URL homologação

--- js/example.js
+++ js/example.js
@@ -2,8 +2,8 @@
  * Created by danielp on 12/2/2014.
  */
 
-//var urlAjax =  "http://hml.bompracredito.com.br/leadgen/lead/<<SEU TOKEN AQUI>>"; // url de homologação
-var urlAjax =  "http://localhost:60494/leadgen/lead/8bf0efc6-8cb7-4884-a338-a4ab009f0df3"; // url de homologação
+var urlAjax =  "http://hml.bompracredito.com.br/app/leadgen/lead/<<SEU TOKEN AQUI>>"; // url de homologaÃ§Ã£o
+//var urlAjax =  "http://localhost:60494/leadgen/lead/8bf0efc6-8cb7-4884-a338-a4ab009f0df3"; // url de homologaÃ§Ã£o
 
 function getRequest() {
     var leadGenRequest = {
